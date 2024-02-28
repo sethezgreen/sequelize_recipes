e89885152f68fa319939e5ce558b0319d6c52efa
sethezgreen <sethezgreen@gmail.com>
2024-02-27 22:26:45 -0500

    created main view and top nav component

---
+++ client/src/views/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './main.css'
+import TopNav from '../components/TopNav'
+
+function Main() {
+    
+    
+    return (
+        <div className='main_view'>
+            <TopNav />
+            {/* sidenav component */}
+        </div>
+    )
+}
+
+export default Main
